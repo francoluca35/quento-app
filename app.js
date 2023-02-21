74a042d36ccb389de0b73035e553aebf4f7b8ffe
francoluca35 <francolucap1@gmail.com>
2023-02-21 20:27:10 -0300

    conexion con mongo db con html y js

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
+const express = require('express')
+const  mongoose = require('mongoose')
+const path = require('path')
 require('dotenv').config()
 
-const express = require('express')
 
 const app = express()
 
+mongoose.connect(
+    'mongodb+srv://FRANCO1234:PeiKcdqzbgHcefe1@developets.pnvogln.mongodb.net/stock-app?retryWrites=true&w=majority'
+    )
+    .then((result)=>{ console.log('conexion exitosa a la MGDB')})
+    .catch((err)=> console.log(err))
+
+    const productSchema = mongoose.Schema(
+    {
+        name:{type:String, require:true},
+        price: Number,
+    },
+    {timestamps:true}
+    )
+
+    const Product = mongoose.model('product', productSchema)
+
+app.use(express.json())
+
+    app.post('/api/v1/products', (req,ren)=>{
+
+        const newProduct=new Product(req.body)
+            
+       newProduct
+       .save()
+       .then( (result)=>{
+        res.status(201).json({ok:true})
+       })
+       .catch((err) => console.log(err))
+
+        
+    })
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res)=>{
     console.log('peticion recibida')
 
-    res.send('hola mundo')
+    res.status(200).sendFile('index.html', {root: __dirname})
 })
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 
     app.listen(PORT, ()=>{
         console.log('holaaa puerto', PORT)
